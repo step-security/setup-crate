449c61e52a41c64a6d604f0d9f93e4bbc9bff283
amanstep <aman@stepsecurity.io>
2025-11-05 12:23:27 +0530

    feat: Initial release

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
 # setup-crate
+
+[![Build Status](https://img.shields.io/github/actions/workflow/status/step-security/setup-crate/build.yaml?branch=trunk)](https://github.com/step-security/setup-crate/actions/workflows/build.yaml)
+
+This GitHub Action will install a release of a Rust crate for you.
+
+## Introduction
+
+This action will work for any project that satisfies the following conditions:
+- The project is a public GitHub project.
+- The project uses GitHub releases with semver tag names.
+- The project attaches assets to the GitHub release that contain a Rust target.
+  The following targets are looked for:
+
+  | Arch    | Node Platform | Targets                                                     |
+  | ------- | ------------- | ----------------------------------------------------------- |
+  | `x64`   | `linux`       | `x86_64-unknown-linux-musl` or `x86_64-unknown-linux-gnu`   |
+  | `x64`   | `darwin`      | `x86_64-apple-darwin`                                       |
+  | `x64`   | `win32`       | `x86_64-pc-windows-msvc`                                    |
+  | `arm64` | `linux`       | `aarch64-unknown-linux-musl` or `aarch64-unknown-linux-gnu` |
+  | `arm64` | `darwin`      | `aarch64-apple-darwin`                                      |
+
+- The asset is a `.tar.gz` or `.zip` archive that contains a binary with the
+  project name.
+
+## Usage
+
+### Examples
+
+In most cases all you will need is to specify `repo` and the `owner/name` of the
+project in your workflow. For example the following installs the latest version
+of [mdBook](https://github.com/rust-lang/mdBook).
+
+```yaml
+- uses: step-security/setup-crate@v1
+  with:
+    repo: rust-lang/mdBook
+```
+
+If you want a specific version you can specify this by suffixing version to the
+input. For example the following installs the latest `0.10.x` version of
+[just](https://github.com/casey/just).
+
+```yaml
+- uses: step-security/setup-crate@v1
+  with:
+    repo: casey/just@0.10
+```
+
+### Inputs
+
+| Name           | Required | Description                                                         | Type   | Default                     |
+| -------------- | -------- | ------------------------------------------------------------------- | ------ | --------------------------- |
+| `repo`         | no       | The GitHub repository name and valid NPM-style semver specification | string |                             |
+| `github-token` | no       | The GitHub token for making API requests                            | string | ${{ secrets.GITHUB_TOKEN }} |
+
+The semver specification is passed directly to NPM's [semver
+package](https://www.npmjs.com/package/semver). This GitHub Action will install
+the latest matching release. Examples include
+
+- `version: '*'` latest version (default).
+- `version: '0.1'` equivalent to `>=0.1.0 <0.2.0`.
+- `version: '0.1.x'` equivalent to `>=0.1.0 <0.2.0`.
+- `version: '0.1.0'` equivalent to `=0.1.0`.
+- `version: '^0.1.0'` equivalent to `>=0.1.0 <0.2.0`.
+
+### Deprecated inputs
+
+The following inputs are still supported for now but will be removed in a future
+release.
+
+| Name      | Required | Description                            | Type   | Default |
+| --------- | -------- | -------------------------------------- | ------ | ------- |
+| `owner`   | no       | The GitHub user or organization name   | string |         |
+| `name`    | no       | The GitHub repository name             | string |         |
+| `version` | no       | A valid NPM-style semver specification | string | *       |
+
+## License
+
+Licensed under MIT license ([LICENSE](LICENSE))
---
+++ setup-crate/src/index.ts
@@ -0,0 +1,214 @@
+import {
+  constants as fs_constants,
+  promises as fs,
+} from 'fs';
+import * as path from 'path';
+import * as semver from 'semver';
+
+import * as core from '@actions/core';
+import * as tc from '@actions/tool-cache';
+import { Octokit } from '@octokit/rest';
+
+/**
+ * @returns {string[]} possible Rust target specifiers for the current platform.
+ */
+function getTargets(): string[] {
+  const { arch, platform } = process;
+  if (arch == "x64") {
+    if (platform == "linux") {
+      return ["x86_64-unknown-linux-musl", "x86_64-unknown-linux-gnu"];
+    } else if (platform == "darwin") {
+      return ["x86_64-apple-darwin"];
+    } else if (platform == "win32") {
+      return ["x86_64-pc-windows-msvc"];
+    }
+  }
+  if (arch == "arm64") {
+    if (platform == "linux") {
+      return ["aarch64-unknown-linux-musl", "aarch64-unknown-linux-gnu"];
+    } else if (platform == "darwin") {
+      return ["aarch64-apple-darwin"];
+    }
+  }
+  throw new Error(
+    `failed to determine any valid targets; arch = ${arch}, platform = ${platform}`,
+  );
+}
+
+/**
+ * Options that this package supports.
+ */
+export interface Options {
+  /** Passed to Octokit auth, typically the GITHUB_TOKEN */
+  auth: any;
+}
+
+/**
+ * Represents a tool to install from GitHub.
+ */
+export interface Tool {
+  /** The GitHub owner (username or organization). */
+  owner: string;
+  /** The GitHub repo name. */
+  name: string;
+  /** A valid semantic version specifier for the tool. */
+  versionSpec?: string;
+  /** The name of the tool binary (defaults to the repo name) */
+  bin?: string;
+}
+
+/**
+ * Represents an installed tool.
+ */
+export interface InstalledTool {
+  /** The GitHub owner (username or organization). */
+  owner: string;
+  /** The GitHub repo name. */
+  name: string;
+  /** The version of the tool. */
+  version: string;
+  /** The directory containing the tool binary. */
+  dir: string;
+  /** The name of the tool binary (defaults to the repo name) */
+  bin?: string;
+}
+
+/**
+ * Represents a single release for a {@link Tool}.
+ */
+interface Release {
+  /** The exact release tag. */
+  version: string;
+  /** The asset download URL. */
+  downloadUrl: string;
+}
+
+/**
+ * Fetch the latest matching release for the given tool.
+ *
+ * @param tool the tool to fetch a release for.
+ *
+ * @returns {Promise<Release>} a single GitHub release.
+ */
+async function getRelease(tool: Tool, options?: Options): Promise<Release> {
+  const targets = getTargets();
+  const { owner, name, versionSpec } = tool;
+  const octokit = new Octokit(options);
+  return octokit
+    .paginate(
+      "GET /repos/{owner}/{repo}/releases",
+      { owner, repo: name },
+      (response, done) => {
+        const releases = response.data
+          .map((rel): Release | undefined => {
+            const asset = rel.assets.find((ass) =>
+              targets.some((target) => ass.name.includes(target)),
+            );
+            if (asset) {
+              return {
+                version: rel.tag_name.replace(/^v/, ""),
+                downloadUrl: asset.browser_download_url,
+              };
+            }
+          })
+          .filter((rel) => Boolean(rel))
+          .filter((rel) =>
+            rel && versionSpec
+              ? semver.satisfies(rel.version, versionSpec)
+              : true,
+          );
+        if (releases.length > 0) {
+          done();
+        }
+        return releases;
+      },
+    )
+    .then((releases) => {
+      const release = releases.find((release) => release != null);
+      if (release === undefined) {
+        throw new Error(
+          `no release for ${name} matching version specifier ${versionSpec}`,
+        );
+      }
+      return release;
+    });
+}
+
+async function handleBadBinaryPermissions(
+  tool: Tool,
+  dir: string,
+): Promise<void> {
+  const { name, bin } = tool;
+  if (process.platform !== "win32") {
+    const findBin = async () => {
+      const files = await fs.readdir(dir);
+      for await (const file of files) {
+        if (file.toLowerCase() == name.toLowerCase()) {
+          return file;
+        }
+      }
+      return name;
+    };
+    const binary = path.join(dir, bin ? bin : await findBin());
+    try {
+      await fs.access(binary, fs_constants.X_OK);
+    } catch {
+      await fs.chmod(binary, "755");
+      core.debug(`Fixed file permissions (-> 0o755) for ${binary}`);
+    }
+  }
+}
+
+/**
+ * Checks the tool cache for the tool, and if it is missing fetches it from
+ * GitHub releases.
+ *
+ * @param tool the tool to check or install.
+ *
+ * @returns the directory containing the tool binary.
+ */
+export async function checkOrInstallTool(
+  tool: Tool,
+  options?: Options,
+): Promise<InstalledTool> {
+  const { name, versionSpec } = tool;
+
+  // first check if we have previously downloaded the tool
+  let dir = tc.find(name, versionSpec || "*");
+
+  if (!dir) {
+    // find the latest release by querying GitHub API
+    const { version, downloadUrl } = await getRelease(tool, options);
+
+    // download, extract, and cache the tool
+    const artifact = await tc.downloadTool(downloadUrl);
+    core.debug(`Successfully downloaded ${name} v${version}`);
+
+    let extractDir;
+    if (downloadUrl.endsWith(".zip")) {
+      extractDir = await tc.extractZip(artifact);
+    } else {
+      extractDir = await tc.extractTar(artifact);
+    }
+    core.debug(`Successfully extracted archive for ${name} v${version}`);
+
+    // handle the case where there is a single directory extracted
+    const files = await fs.readdir(extractDir);
+    if (files.length == 1) {
+      const maybeDir = path.join(extractDir, files[0]);
+      if ((await fs.lstat(maybeDir)).isDirectory()) {
+        extractDir = maybeDir;
+      }
+    }
+
+    dir = await tc.cacheDir(extractDir, name, version);
+
+    // handle bad binary permissions, the binary needs to be executable!
+    await handleBadBinaryPermissions(tool, dir);
+  }
+
+  // FIXME: is there a better way to get the version?
+  const version = path.basename(path.dirname(dir));
+
+  return { version, dir, ...tool };
+}
